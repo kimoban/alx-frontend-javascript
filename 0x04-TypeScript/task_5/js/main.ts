e8aa41165ebc40e79f208d5b9fa3049d873559b2
Isaiah Kimoban <67864381+kimoban@users.noreply.github.com>
2025-07-13 22:06:11 +0000

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,17 +1,17 @@
-interface MajorCredits {
+export interface MajorCredits {
   credits: number;
   brand: "Major";
 }
 
-interface MinorCredits {
+export interface MinorCredits {
   credits: number;
   brand: "Minor";
 }
 
-function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
   return { credits: subject1.credits + subject2.credits, brand: "Major" };
 }
 
-function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
   return { credits: subject1.credits + subject2.credits, brand: "Minor" };
 }
